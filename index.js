5353c79102bbf8429a94aca8f2bde62f29a220be
jaythale <jaythale@123.com>
2024-09-27 12:49:35 +0530

    initial setup

--- index.js
+++ index.js
@@ -11,7 +11,12 @@ const app = express()
 app.use(express.json())
 app.use(express.static("dists"))
 app.use(cookieParser())
-app.use(cors({ origin: "http://localhost:5173", credentials: true }))
+app.use(cors({
+    origin: process.env.NODE_ENV === "development"
+        ? "http://localhost:5173"
+        : "https://full-portfolio-c2f0.onrender.com",
+    credentials: true
+}))
 
 app.use("/api/auth", require("./routes/auth.routes"))
 app.use("/api/admin", adminProtected, require("./routes/admin.routes"))
